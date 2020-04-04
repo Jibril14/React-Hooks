2f30440d81ce350ecc084dab6569807b5d1400c7
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2020-04-04 23:40:30 +0100

    Using UseEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -6,10 +6,30 @@ import Search from "./Search";
 
 function Ingredients() {
     const [userIngredients, setUserIngredient] = useState([]);
+
     const addIngredientHandler = (ingredient) => {
         setUserIngredient([{ id: Math.random(), ...ingredient }]);
     };
 
+    /**
+    // Using useEffect to fetch data from database
+    // Set Our state from Db data rather than manually from another's input element Component
+    useEffect(() => {
+        fetch("api/ingredient/get")
+            .then((response) => response.json())
+            .then((responseData) => {
+                const ingredientFromDb = [];
+                for (const ing in responseData) {
+                    ingredientFromDb.push({
+                        id: ing,
+                        title: responseData[ing].title,
+                        amount: responseData[ing].amount
+                    });
+                }
+                setUserIngredient(ingredientFromDb);
+            });
+    }, []);
+**/
     return (
         <div className="App">
             <IngredientForm onAddIngredient={addIngredientHandler} />
