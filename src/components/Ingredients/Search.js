12b62d28a1f8f04e32c2ca0e13b9e786bc82ea6d
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2020-04-08 11:23:34 +0100

    Using useRef

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,33 +6,45 @@ import "./Search.css";
 const Search = React.memo((props) => {
     const [enteredSearch, setEnteredSearch] = useState([]);
     const { onloadIngs } = props;
-    useEffect(() => {
-        const query =
-            enteredSearch.length === 0
-                ? ""
-                : `?orderBy="title"&equalTo="${enteredSearch}`;
-        fetch("http://store.com/api/ingredient/ingredient.json" + query)
-            .then((response) => response.json())
-            .then((responseData) => {
-                const ingredientFromDb = [];
-                for (const ing in responseData) {
-                    ingredientFromDb.push({
-                        id: ing,
-                        title: responseData[ing].title,
-                        amount: responseData[ing].amount
-                    });
-                }
 
-                onloadIngs(ingredientFromDb);
-            })
-            .catch((error) => {});
+    const inputRef = useRef();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (enteredSearch === inputRef.current.value) {
+                const query =
+                    enteredSearch.length === 0
+                        ? ""
+                        : `?orderBy="title"&equalTo="${enteredSearch}`;
+                fetch("http://store.com/api/ingredient/ingredient.json" + query)
+                    .then((response) => response.json())
+                    .then((responseData) => {
+                        const ingredientFromDb = [];
+                        for (const ing in responseData) {
+                            ingredientFromDb.push({
+                                id: ing,
+                                title: responseData[ing].title,
+                                amount: responseData[ing].amount
+                            });
+                        }
+
+                        onloadIngs(ingredientFromDb);
+                    })
+                    .catch((error) => {});
+            }
+        }, 500);
+        return () => {
+            clearTimeout(timer);
+        };
     }, [enteredSearch, onloadIngs]);
+
     return (
         <section className="search">
             <Card>
                 <div className="search-input">
                     <label>Filter by Title</label>
                     <input
+                        ref={inputRef}
                         type="text"
                         value={enteredSearch}
                         onChange={(event) =>
